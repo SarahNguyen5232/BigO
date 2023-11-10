2c5f15cbb535067eafcd2c9116794e5a86958be5
SarahNguyen5232 <148515397+SarahNguyen5232@users.noreply.github.com>
2023-11-09 18:53:36 -0800

    Add files via upload

---
+++ big.js
@@ -0,0 +1,31 @@
+// O(n)
+// O(n)
+// O(1)
+// O(n^3)
+// O(n)
+// O(n)
+// O(n log n)
+// O(2^n)
+// O(1)
+// O(n^2)
+
+// O(n)
+// O(n)
+// O(1)
+// O(n)
+// O(n^2)
+// O(n)
+
+//  True
+//  True
+//  False
+//  O(n)
+//  O(n)
+// O(n) at least (depends on what the callback does)
+// O(n log n)
+// O(n)
+// O(1)
+// O(n) it can be O(1) if the end, but we can’t assume that
+// O(1)
+// O(n)
+// O(n)
